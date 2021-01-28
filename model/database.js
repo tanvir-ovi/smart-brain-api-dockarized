966e28e530b6042e0e4225bf6b38cb6b8d26b1b0
tanvir-ovi <tanvirovi6@gmail.com>
2021-01-28 22:22:59 +0600

    set database for requirements of heroku

--- model/database.js
+++ model/database.js
@@ -2,7 +2,7 @@ const knex = require('knex');
 
 //database connections
 const db = knex({
-  connectionString: process.env.DATABASE_URL,
+  client: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false
   }
