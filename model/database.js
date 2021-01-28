ddfe85b2c739bd582c5b439b9f56f4d71d97b056
tanvir-ovi <tanvirovi6@gmail.com>
2021-01-28 22:14:19 +0600

    set database for requirements of heroku

--- model/database.js
+++ model/database.js
@@ -2,12 +2,9 @@ const knex = require('knex');
 
 //database connections
 const db = knex({
-  client: 'pg',
-  connection: {
-    host: '127.0.0.1',
-    user: 'postgres',
-    password: 'khankirpola06',
-    database: 'smartBrainDB'
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
   }
 }); 
 
